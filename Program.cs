f272979dd389d928c7a0b12931b87829ccef821a
sayojj <ssfaksjkwq@gmail.com>
2023-05-12 13:53:48 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manati daxil ed");
+
+            double dollar = Convert.ToDouble(Console.ReadLine());
+   
+            double manat = dollar * 1.7 ;
+            double euro = manat * 2;
+            File.AppendAllText(@"Text",$"\ndollar {dollar}   -  manat {manat}");
+        }
+
+    }
+
+}
